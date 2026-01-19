bd47b77daa075a0483943fe0c929fbc9b93c03be
Icyhuman <jorgen.storum@gmail.com>
2026-01-19 19:01:37 +0100

    man setting this up is annoying

---
+++ src/enhancements.lua
@@ -0,0 +1,17 @@
+SMODS.Atlas{
+    key = 'Green',
+    path = 'green.png',
+    px = 71,
+    py = 95,
+}
+
+-- Green
+SMODS.Enhancement {
+    key = 'green',
+    atlas = 'Green',
+    pos = { x = 0, y = 0 },
+    loc_txt= {
+        name = 'Green',
+        text = { 'Has been {C:green}Greened{}' }
+    },
+}
